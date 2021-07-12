8e8940b771540c2403e1c51dbe006d077572e925
Luana GD <lmtn2706@gmail.com>
2021-07-12 16:23:40 +0200

    Inté du fichier Ink ;  premiers script en test

--- Ink-Project/Assets/Scripts/tutorialScript.cs
+++ Ink-Project/Assets/Scripts/tutorialScript.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
 
 public class tutorialScript : MonoBehaviour
 {
+
+    public TextAsset inkJSON;
+    private Story story;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        story = new Story(inkJSON.text);
+        Debug.Log(story.Continue());
     }
 
     // Update is called once per frame
